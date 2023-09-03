688ffd046f6f652fecf5bd49e4f3ee532f63742c
Alexis Sayago <as.sayagom@gmail.com>
2023-09-03 17:56:37 -0300

    curso de angular hasta la sección 6

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,34 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+
+  @Input()
+  public characterList: Character[] = [{
+    name: 'Trunks',
+    power: 10
+  }]
+
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+  deleteCharacterById(id?:string):void {
+
+    if (!id) return;
+
+    this.onDelete.emit(id)
+  }
+
+  // @Output()
+  // public onDelete: EventEmitter<number> = new EventEmitter();
+
+  // onDeleteCharacter(index:number):void {
+  //   this.onDelete.emit(index)
+  // }
+
+}
